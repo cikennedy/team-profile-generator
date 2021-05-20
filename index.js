d93d7d95aebedb6c4f7caef628c17d225d3eba0e
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-20 13:15:52 -0500

    created initial constructor function in the employee.js file

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, 
