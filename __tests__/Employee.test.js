28c4880c000d8858c330f31c537ef441b753cf48
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-28 10:51:11 -0500

    added example test in employeetest file, will rework for current js file and remaining others

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,16 @@
+const { ExpectationFailed } = require('http-errors');
+const { iteratee } = require('lodash');
 const Employee = require('../lib/Employee');
+
+// Example Test Below, rework this 
+describe("function name", () => {
+    it("does this", () => {
+        const employee = new Employee("John");
+
+        ExpectationFailed(employee.letters).toEqual(
+            expect.arrayContaining([expect.objectContaining({ char: "J"})])
+        );
+
+        expect(employee.letters[0]).toBeInstanceOf(Employee);
+    });
+})
